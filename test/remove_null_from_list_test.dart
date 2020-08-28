317cba6f3627e0b906b2dbb9ce3815bb098e48c3
James Alexander <j.alex@email.com>
2020-08-28 00:59:07 -0700

    Wrote the test.

--- test/remove_null_from_list_test.dart
+++ test/remove_null_from_list_test.dart
@@ -1,7 +1,19 @@
+import 'dart:math';
 import 'package:test/test.dart';
 import 'package:remove_null_from_list/remove_null_from_list.dart';
 
 void main() {
-  // TODO: Generate lists with null values assigned randomly, call the method,
-  // check if the lists had all of their null values removed.
+  test('Remove Null Values', () {
+    final random = Random();
+
+    for (var i = 0; i < 100; i++) {
+      final list = List<int>.generate(100, (index) => random.nextBool() ? i : null);
+
+      final nullValues = list.where((element) => element == null).length;
+
+      list.removeNull();
+
+      expect(list.length, equals(100 - nullValues));
+    }
+  });
 }
